11a7063527a945cf5e4000b765fb4c2db50d9ad6
Austin Hollenbaugh <austinhollenbaugh@gmail.com>
2016-12-06 23:26:02 -0700

    added google maps geolocation

--- components/home/homeCtrl.js
+++ components/home/homeCtrl.js
@@ -2,7 +2,17 @@ angular.module("weatherApp").controller("homeCtrl", function($scope, homeService
   $scope.getForecast = function(location) {
     console.log(location);
     $scope.location = "";
-    homeService.geoCode(location);
+    homeService.geoCode(location).then(function(response) {
+      console.log(response);
+      var lat = response.lat;
+      var lng = response.lng;
+
+      homeService.getForecast(lat, lng).then(function(response) {
+        console.log(response);
+        $scope.temp = response.currently.temperature;
+        $scope.summary = response.currently.summary;
+      });
+    });
   };
 
 
--- components/home/homeService.js
+++ components/home/homeService.js
@@ -3,26 +3,27 @@ angular.module("weatherApp").service("homeService", function($http) {
   var darkSkyKey = "54f2199c7f2eeef83b7d85834c79a5c3";
   var googleMapsKey = "AIzaSyBCQvwyLahB8OTK-BZuAksotjpP1yQs8tc";
 
+  this.getForecast = function(lat, lng) {
+    return $http({
+      method: 'GET',
+      url: 'https://api.darksky.net/forecast/' + darkSkyKey + '/'+ lat + ',' + lng
+    }).then(function(response) {
+      // console.log(response.data);
+      return response.data;
+    });
+  };
+
   this.geoCode = function(address) {
     return $http({
       method: 'GET',
       url:'https://maps.googleapis.com/maps/api/geocode/json?address=' +
             address + '&key=' + googleMapsKey
     }).then(function(response) {
-      console.log("lat", response.data.results[0].geometry.location.lat);
-      console.log("lng", response.data.results[0].geometry.location.lng);
+      return response.data.results[0].geometry.location;
     });
   };
 
 
-  this.getData = function(location) {
 
-    return $http({
-      method: 'GET',
-      url: 'https://api.darksky.net/forecast/' + darkSkyKey + '/37.8267,-122.4233'
-    }).then(function(response) {
-      console.log(response);
-    });
-  };
 
 }); //end service
